63ae39bfd074a77c184205caf90e16f3c5e0ba05
THILAGAVATHI.K <lillyskct@gmail.com>
2024-05-17 20:56:25 +0530

    new commit

--- demo/SELE WEBDRIVER(DAILY CHALLENGE)/src/test/java/com/example/AppTest.java
+++ demo/SELE WEBDRIVER(DAILY CHALLENGE)/src/test/java/com/example/AppTest.java
@@ -1,20 +1,32 @@
 package com.example;
-
-import static org.junit.Assert.assertTrue;
-
+ import java.net.MalformedURLException;
+import java.net.URL;
 import org.junit.Test;
-
-/**
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
+ /**
  * Unit test for simple App.
  */
-public class AppTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
-    @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
-    }
+ public class AppTest 
+ {
+ public static WebDriver driver;
+@Test
+ public void shouldAnswerWithTrue() throws MalformedURLException, InterruptedException
+ {
+ChromeOptions co = new ChromeOptions();
+// WebDriverManager.chromedriver().setup();
+// WebDriver driver = new ChromeDriver();
+driver= new RemoteWebDriver(new URL("http://localhost:4444/"),co);
+driver.get("https://www.shoppersstop.com");
+Thread.sleep(8000);
+driver.findElement(By.className("user-icon")).click();
+driver.manage().window().maximize();
+// String tit =driver.getTitle();
+// String src =driver.getPageSource();
+driver.navigate().to("https://www.google.com");
+driver.navigate().back();
+driver.close();
 }
+ }
